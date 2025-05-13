3e74efb052e9bdbca0791743fef4bf4a816ff572
Mansioqt <153075222+Mansioqt@users.noreply.github.com>
2025-05-13 11:35:49 +0700

    flask setup for deployment

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template, jsonify, request
-from datetime import datetime
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-connection_string = 'mongodb+srv://test:sparta@cluster0.gjjdcem.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-client = MongoClient(connection_string)
-db = client.dbsparta
+# Load environment variables
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+# MongoDB client setup
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+# Flask app setup
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive,
+    }
+    
+    db.homework.insert_one(doc)
+    return jsonify({'msg': 'POST request successful!'})
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.homework.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
